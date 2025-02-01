805f9dc08e056d63c6b3f39b0243076985aeeb98
FlorentCollin <florent.collin@necko.tech>
2025-02-01 12:12:50 +0100

    style: add biome

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,30 +1,33 @@
-import { apiReference } from '@scalar/hono-api-reference';
-import { Hono } from 'hono'
-import { openAPISpecs } from 'hono-openapi';
+import { apiReference } from "@scalar/hono-api-reference";
+import { Hono } from "hono";
+import { openAPISpecs } from "hono-openapi";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
-
-app.get(
-  '/openapi',
-  openAPISpecs(app, {
-    documentation: {
-      info: { title: 'Hono API', version: '1.0.0', description: 'Greeting API' },
-      servers: [{ url: 'http://localhost:3000', description: 'Local Server' }],
-    },
-  })
-);
-
+app.get("/", (c) => {
+	return c.text("Hello Hono!");
+});
 
 app.get(
-  '/docs',
-  apiReference({
-    theme: 'saturn',
-    spec: { url: '/openapi' },
-  })
+	"/openapi",
+	openAPISpecs(app, {
+		documentation: {
+			info: {
+				title: "Hono API",
+				version: "1.0.0",
+				description: "Greeting API",
+			},
+			servers: [{ url: "http://localhost:3000", description: "Local Server" }],
+		},
+	}),
 );
 
-export default app
+app.get(
+	"/docs",
+	apiReference({
+		theme: "saturn",
+		spec: { url: "/openapi" },
+	}),
+);
+
+export default app;
