f662cebb6cc3487cf764c74120d34febe590b7d1
FlorentCollin <florent.collin@necko.tech>
2025-02-01 11:59:26 +0100

    feat: initial monorepo setup

---
+++ packages/api/src/index.ts
@@ -0,0 +1,30 @@
+import { apiReference } from '@scalar/hono-api-reference';
+import { Hono } from 'hono'
+import { openAPISpecs } from 'hono-openapi';
+
+const app = new Hono()
+
+app.get('/', (c) => {
+  return c.text('Hello Hono!')
+})
+
+app.get(
+  '/openapi',
+  openAPISpecs(app, {
+    documentation: {
+      info: { title: 'Hono API', version: '1.0.0', description: 'Greeting API' },
+      servers: [{ url: 'http://localhost:3000', description: 'Local Server' }],
+    },
+  })
+);
+
+
+app.get(
+  '/docs',
+  apiReference({
+    theme: 'saturn',
+    spec: { url: '/openapi' },
+  })
+);
+
+export default app
